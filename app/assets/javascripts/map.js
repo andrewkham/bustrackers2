f67bc8f23c17334df48242c8c9e3994ca3d2805c
Andrew Ham <hampossible@gmail.com>
2015-07-14 12:32:10 -0700

    can search for routes and render on map

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,6 +1,50 @@
 // The init function needs to run on load
 google.maps.event.addDomListener(window, 'load', initialize);
 google.maps.event.addDomListener(window, 'page:load', initialize);
+var my_map;
+
+function selectRoute(){
+  document.getElementById('submitText').addEventListener("click", submitRoute);
+};
+
+function getMap(my_map) {
+  return my_map;
+}
+
+function getStopsData(x) {
+  //API call for stops
+  var stops = [];
+  $.ajax("http://api.metro.net/agencies/lametro/routes/" + x + "/stops/", {
+    success: function(data) {
+      stops = data;
+      console.log(stops.items[0].latitude);
+
+      for (var i = 0; i < stops.items.length; i++) {
+        var stopPositions = new google.maps.LatLng(stops.items[i].latitude, stops.items[i].longitude);
+
+        var stopMarker = new google.maps.Marker({
+          position: stopPositions,
+          icon: {
+            path: google.maps.SymbolPath.CIRCLE,
+            scale: 3,
+            fillColor: '#FF0000',
+            strokeColor: '#FF0000',
+            fillOpacity: 1,
+          }
+        });
+        console.log(stopMarker);
+        console.log(my_map)
+        stopMarker.setMap(my_map);
+      }
+    }
+
+  });
+}
+
+function submitRoute() {
+  var myRoute = document.getElementById('inputText').value;
+  getStopsData(myRoute);
+};
 
 // Initialize Google Map
 function initialize() {
@@ -11,44 +55,16 @@ function initialize() {
     center: new google.maps.LatLng(34.0218628, -118.4804206),
     zoom: 12
   }
-
   // Define my_map to be map rendered on index page, labeled as "address-map"
-  var my_map = new google.maps.Map(document.getElementById("address-map"), mapProperties);
+  my_map = new google.maps.Map(document.getElementById("address-map"), mapProperties);
 
   // Defines variable "bounds" as the LatLngBounds of our map
-  var bounds = new google.maps.LatLngBounds();
+  // var bounds = new google.maps.LatLngBounds();
   // Loop for stop positions
 
-  //API call for stops
-  var stops = [];
-  $.ajax("http://api.metro.net/agencies/lametro/routes/704/stops/", {
-    success: function(data) {
-      stops = data;
-      console.log(stops)
-      console.log(stops.items[0].latitude);
+  // Fits bounds of the map according to LatLngbounds of our map as defined by coordinates of existing data
+  // my_map.fitBounds(bounds);
 
-      for (var i = 0; i < stops.items.length; i++) {
-        var stopPositions = new google.maps.LatLng(stops.items[i].latitude, stops.items[i].longitude);
-        // console.log(stopPositions);
-        var stopMarker = new google.maps.Marker({
-          position: stopPositions,
-          icon: {
-            path: google.maps.SymbolPath.CIRCLE,
-            scale: 3,
-            fillColor: '#FF0000',
-            strokeColor: '#FF0000',
-            fillOpacity: 1,
-          }
-        })
-        stopMarker.setMap(my_map);
+  selectRoute();
 
-        bounds.extend(stopPositions);
-      }
-
-
-    }
-  });
-
-// Fits bounds of the map according to LatLngbounds of our map as defined by coordinates of existing data
-// my_map.fitBounds(bounds);
 }
